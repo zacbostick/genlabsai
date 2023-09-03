a24ce366e0650645eec37d34dc4ed78768ba936b
Zac <zac.bostick@gmail.com>
2023-09-03 10:42:26 -0500

    Mobile Sizing fix

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -10,7 +10,7 @@ export const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="text-white font-bold py-36 text-center space-y-5 mt-24">
+    <div className="text-white font-bold py-36 text-center space-y-5 sm:mt-12 mt-24">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Ultimate AI Platform for...</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#6dffee] via-[#0eedf4] to-[#0eedf4] text-[#5CE2E7] pb-2">
